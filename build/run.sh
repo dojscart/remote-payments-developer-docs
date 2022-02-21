1a5f42a567655af37d6e46063e99b892531c9af5
Roman Kondratyev <roman.kondratyev@paymentsense.com>
2022-02-21 20:48:25 +0100

    Fixed prod build

--- build/run.sh
+++ build/run.sh
@@ -14,18 +14,12 @@ else
 fi
 
 if [ $ENVIRONMENT == "production" ]; then
-    echo "Production build detected."
+    echo "Production build detected!"
     sed -i "s#url: 'https://dev-docs.dojo.dev'#url: 'https://docs.dojo.tech'#g" $filename
-    cat $filename
-    exit 0
-fi
-
-# Fix for master in dev?
-if [ $ENVIRONMENT == "master" ]; then
+else if [ $ENVIRONMENT == "master" ]; then
     echo "Staging build detected!"
     sed -i "s#url: 'https://dev-docs.dojo.dev'#url: 'https://staging-docs.dojo.dev'#g" $filename
     sed -i "s#baseUrl: '/'#baseUrl: '/master/'#g" $filename
- 
 else
     echo "Dev build detected with '$ENVIRONMENT'!"
     sed -i "s#baseUrl: '/'#baseUrl: '/$ENVIRONMENT/'#g" $filename
