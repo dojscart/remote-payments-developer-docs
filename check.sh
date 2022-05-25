cd2d1d6eaa44972f4eed7c741f7b76dd0d2a9095
myafka <sorry88@bk.ru>
2022-05-25 16:59:05 +0100

    updated the script

--- check.sh
+++ check.sh
@@ -1,6 +1,14 @@
-urls=$(curl -s https://docs.dojo.tech/sitemap.xml | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
-for i in $urls
+#!/bin/bash
+
+declare -a urls=("https://docs.dojo.tech/")
+for url in "${urls[@]}"
 do
-  lychee -v $i
-  print i
+    url=$(curl -s https://docs.dojo.tech/sitemap.xml | grep "<loc>" | awk -F"<loc>" '{print $2} ' | awk -F"</loc>" '{print $1}')
+    urls+=( "url" )
+done
+
+for url in "${urls[@]}"
+do 
+    echo "$url"
+    lychee -v $url
 done
